14320c71242f10792357ea018d9c6aa8e4ee0c78
Jim Heising <jheising@redfoundry.com>
2014-08-21 20:58:18 -0700

    Changed throttle rate

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.fetch_regex = /^\/fetch\/(.*)$/; // The URL to look for when parsing the
 exports.proxy_request_timeout_ms = 10000; // The lenght of time we'll wait for a proxy server to respond before timing out.
 exports.max_request_length = 100000; // The maximum length of characters allowed for a request or a response.
 exports.enable_rate_limiting = true;
-exports.max_requests_per_second = 1; // The maximum number of requests per second to allow from a given IP.
+exports.max_requests_per_second = 10; // The maximum number of requests per second to allow from a given IP.
